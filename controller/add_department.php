662793763e3664813ee9e856541184e7b764f820
Md Ehsanul Haque <ehsanul.siamdev@gmail.com>
2026-01-07 05:20:23 +0600

    F04S02: Add new department

--- controller/add_department.php
+++ controller/add_department.php
@@ -1,28 +1,41 @@
 <?php
-session_start();
+require_once('adminCheck.php');
 require_once('../model/departmentModel.php');
 
 if (isset($_POST['submit'])) {
     $department_name = $_POST['department_name'];
-    $description = $_POST['description'];
+    $description = isset($_POST['description']) ? $_POST['description'] : '';
 
     if ($department_name == "") {
         echo "Department name is required";
+        echo "<br><a href='../view/admin_department_add.php'>Go Back</a>";
+        exit;
+    }
+
+    $existing = getDepartmentByName($department_name);
+    if ($existing) {
+        echo "Department '$department_name' already exists!";
+        echo "<br><a href='../view/admin_department_add.php'>Go Back</a>";
+        exit;
+    }
+
+    $department = [
+        'department_name' => $department_name,
+        'description' => $description
+    ];
+
+    $status = addDepartment($department);
+
+    if ($status) {
+        header('location: ../view/admin_department_list.php');
+        exit;
     } else {
-        $department = [
-            'department_name' => $department_name,
-            'description' => $description
-        ];
-
-        $status = addDepartment($department);
-
-        if ($status) {
-            header('location: ../view/admin_department_list.php');
-        } else {
-            echo "Failed to add department";
-        }
+        echo "Failed to add department";
+        echo "<br><a href='../view/admin_department_add.php'>Go Back</a>";
+        exit;
     }
 } else {
     header('location: ../view/admin_department_add.php');
+    exit;
 }
 ?>
