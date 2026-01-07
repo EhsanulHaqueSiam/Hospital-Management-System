4c132d586de2f7c4f992022bd14b5da0efc585d1
Md Ehsanul Haque <ehsanul.siamdev@gmail.com>
2026-01-07 06:56:29 +0600

    F04S04: Implement Delete Department controller

--- controller/delete_department.php
+++ controller/delete_department.php
@@ -1,9 +1,17 @@
 <?php
-session_start();
+require_once('adminCheck.php');
 require_once('../model/departmentModel.php');
+require_once('../model/doctorModel.php');
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
+
+    $doctors = getDoctorsByDepartment($id);
+    if (count($doctors) > 0) {
+        echo "Cannot delete department: " . count($doctors) . " doctor(s) are assigned to it. Please reassign them first.";
+        echo "<br><a href='../view/admin_department_list.php'>Go Back</a>";
+        exit;
+    }
 
     $status = deleteDepartment($id);
 
@@ -11,6 +19,7 @@ if (isset($_GET['id'])) {
         header('location: ../view/admin_department_list.php');
     } else {
         echo "Failed to delete department";
+        echo "<br><a href='../view/admin_department_list.php'>Go Back</a>";
     }
 } else {
     header('location: ../view/admin_department_list.php');
