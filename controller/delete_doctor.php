2f755716dce46f05f15d7cd65ba50ef9ff4967fe
Md Ehsanul Haque <ehsanul.siamdev@gmail.com>
2026-01-07 06:56:45 +0600

    F05S04: Implement Delete Doctor functionality

--- controller/delete_doctor.php
+++ controller/delete_doctor.php
@@ -1,18 +1,48 @@
 <?php
 session_start();
 require_once('../model/doctorModel.php');
+require_once('../model/userModel.php');
+require_once('../model/appointmentModel.php');
+require_once('../model/prescriptionModel.php');
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
+    $doctor = getDoctorById($id);
+    if (!$doctor) {
+        echo "Doctor not found";
+        echo "<br><a href='../view/admin_doctor_list.php'>Go Back</a>";
+        exit;
+    }
+
+    $user_id = $doctor['user_id'];
+
+    $appointmentCount = countAppointmentsByDoctor($id);
+    if ($appointmentCount > 0) {
+        echo "Cannot delete doctor: " . $appointmentCount . " appointment(s) are linked. Please delete or reassign appointments first.";
+        echo "<br><a href='../view/admin_doctor_list.php'>Go Back</a>";
+        exit;
+    }
+
+    $prescriptionCount = countPrescriptionsByDoctor($id);
+    if ($prescriptionCount > 0) {
+        echo "Cannot delete doctor: " . $prescriptionCount . " prescription(s) are linked. Please delete prescriptions first.";
+        echo "<br><a href='../view/admin_doctor_list.php'>Go Back</a>";
+        exit;
+    }
+
     $status = deleteDoctor($id);
 
     if ($status) {
+        deleteUser($user_id);
         header('location: ../view/admin_doctor_list.php');
+        exit;
     } else {
         echo "Failed to delete doctor";
+        echo "<br><a href='../view/admin_doctor_list.php'>Go Back</a>";
     }
 } else {
     header('location: ../view/admin_doctor_list.php');
+    exit;
 }
 ?>
