844a68d1d3bd40ff5653c02190839a90b45f696e
paromitaprogga10-lgtm <paromitaprogga10@gmail.com>
2026-01-07 09:03:56 +0600

    create notice

--- Progga/controller/notice_controller.php
+++ Progga/controller/notice_controller.php
@@ -1,10 +1,23 @@
 <?php
 session_start();
-require_once('../model/notice_model.php');
+require_once('../models/notice_model.php');
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
 if ($action == 'index') {
     $notices = getAllNotices();
     require_once('../view/notice/index.php');
+} elseif ($action == 'create') {
+    require_once('../view/notice/create.php');
+} elseif ($action == 'store') {
+    $notice = [
+        'title' => $_POST['title'],
+        'content' => $_POST['content'],
+        'category' => $_POST['category'],
+        'is_important' => isset($_POST['is_important']) ? 1 : 0,
+        'expiry_date' => $_POST['expiry_date'],
+        'created_by' => $_SESSION['user_id']
+    ];
+    insertNotice($notice);
+    header("Location: notice_controller.php");
 }
