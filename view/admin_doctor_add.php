bcf30091bed54dbfdc02e37ffec47d646ee7579d
Md Ehsanul Haque <ehsanul.siamdev@gmail.com>
2026-01-07 06:56:38 +0600

    F05S02: Implement Add Doctor functionality

--- view/admin_doctor_add.php
+++ view/admin_doctor_add.php
@@ -1,5 +1,8 @@
 <?php
 require_once('../controller/adminCheck.php');
+require_once('../model/departmentModel.php');
+
+$departments = getAllDepartments();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,74 +23,45 @@ require_once('../controller/adminCheck.php');
     <div class="main-container">
         <h2>Add New Doctor</h2>
 
-        <span class="success-message"></span>
-
-        <form method="POST" action="../controller/add_doctor.php" enctype="multipart/form-data"
-            onsubmit="return validateDoctor()">
+        <form method="POST" action="../controller/add_doctor.php" enctype="multipart/form-data">
             <fieldset>
                 <legend>Doctor Information</legend>
                 <table>
                     <tr>
                         <td>Full Name:</td>
-                        <td><input type="text" name="full_name" onblur="validateDoctorNameBlur()" required></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><span class="error-message" id="fullname-error"></span></td>
+                        <td><input type="text" name="full_name" required></td>
                     </tr>
                     <tr>
                         <td>Email:</td>
-                        <td><input type="email" name="email" onblur="validateDoctorEmailBlur()" required></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><span class="error-message" id="email-error"></span></td>
+                        <td><input type="email" name="email" required></td>
                     </tr>
                     <tr>
                         <td>Phone:</td>
-                        <td><input type="tel" name="phone" onblur="validateDoctorPhoneBlur()" required></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><span class="error-message" id="phone-error"></span></td>
+                        <td><input type="tel" name="phone" required></td>
                     </tr>
                     <tr>
                         <td>Username:</td>
-                        <td><input type="text" name="username" onblur="validateDoctorUsernameBlur()" required></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><span class="error-message" id="username-error"></span></td>
+                        <td><input type="text" name="username" required></td>
                     </tr>
                     <tr>
                         <td>Password:</td>
-                        <td><input type="password" name="password" onblur="validateDoctorPasswordBlur()" required></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><span class="error-message" id="password-error"></span></td>
+                        <td><input type="password" name="password" required></td>
                     </tr>
                     <tr>
                         <td>Department:</td>
                         <td>
                             <select name="department_id" required>
                                 <option value="">-- Select --</option>
-                                <option value="1">Cardiology</option>
+                                <?php foreach ($departments as $dept): ?>
+                                    <option value="<?php echo $dept['id']; ?>"><?php echo $dept['department_name']; ?>
+                                    </option>
+                                <?php endforeach; ?>
                             </select>
                         </td>
                     </tr>
                     <tr>
-                        <td></td>
-                        <td><span class="error-message" id="department-error"></span></td>
-                    </tr>
-                    <tr>
                         <td>Specialization:</td>
-                        <td><input type="text" name="specialization" onblur="validateSpecializationBlur()" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><span class="error-message" id="specialization-error"></span></td>
+                        <td><input type="text" name="specialization" required></td>
                     </tr>
                     <tr>
                         <td>Bio:</td>
@@ -108,7 +82,7 @@ require_once('../controller/adminCheck.php');
                     <tr>
                         <td></td>
                         <td>
-                            <input type="submit" name="create_doctor" value="Create Doctor">
+                            <input type="submit" name="submit" value="Create Doctor">
                             <a href="admin_doctor_list.php"><button type="button">Cancel</button></a>
                         </td>
                     </tr>
@@ -116,8 +90,6 @@ require_once('../controller/adminCheck.php');
             </fieldset>
         </form>
     </div>
-
-    <script src="../assets/js/admin.js"></script>
 </body>
 
 </html>
