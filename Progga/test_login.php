58022d8985242e177910324be65c5449adf31cee
paromitaprogga10-lgtm <paromitaprogga10@gmail.com>
2026-01-21 11:13:08 +0600

    Remove duplicate validator files, test_login.php, and add comprehensive audit report

--- Progga/test_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-$_SESSION['user_id'] = 1;
-$_SESSION['role'] = 'Admin'; 
-
-echo "Login successful.<br>";
-echo "<a href='controller/notice_controller.php'>Go to Notice Board</a>";
